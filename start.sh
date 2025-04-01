ea4310a29b84b9d0db70b320271f521e8c0cfa5b
Nicholas <54176339+Nicholas-cool@users.noreply.github.com>
2025-04-01 23:07:42 +0800

    Update start.sh
    修正了启动的 bash 脚本。

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 以后台进程运行 Django 服务器
-python DjangoPart/manage.py runserver 0.0.0.0:9012 &
+python3 DjangoPart/manage.py runserver 0.0.0.0:9012 &
 
 # 等待 Django 服务器启动
-sleep 2
+sleep 5
 
 # 显示浏览器窗口
-open -a "Firefox" "http://127.0.0.1:9022/04money_arrange/#bill_subpage"
+open -a "Google Chrome" "http://127.0.0.1:9012/04money_arrange/#bill_subpage"
